8ef840a40b62fdb4ec524c1a10f1d6250dab9758
Michael Victor <radmakson@gmail.com>
2023-10-18 11:59:47 +0100

    done

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function checkEligibility() {
     const age = parseInt(document.getElementById("age").value);
+    const text = document.getElementById("text").value;
 
     if (isNaN(age)) {
-        document.getElementById("result").innerHTML = "please enter a valid age."
+        document.getElementById("result").innerHTML = "Please enter a valid age."
     } else if (age >= 18){
-        document.getElementById("result").innerHTML ="you are eligible to vote sharp guy!"
+        document.getElementById("result").innerHTML = `You are eligible to vote ${text}. sharp guy!`
     } else {
-        document.getElementById("result").innerHTML ="you are not eligible to vote.. grow up youngie!"
+        document.getElementById("result").innerHTML = `You are not eligible to vote ${text}.. grow up youngie!`
     }
 }
